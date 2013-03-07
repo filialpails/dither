4cc8c22721e21bc44a38d6b46fde121d36664a28
Rob Steward <bobert_1234567890@hotmail.com>
2013-03-07 14:37:58 -0500

    add chrome, opera, IE compatibility, optimize algorithm

--- dither.js
+++ dither.js
@@ -1,6 +1,12 @@
 (function($) {
 	"use strict";
 	$.fn.dither = function() {
+		var threshold_map = [];
+		for (var p = 0; p < 64; ++p) {
+			var q = p ^ (p >> 3);
+			threshold_map[p] = (((p & 4) >> 2) | ((q & 4) >> 1) | ((p & 2) << 1) | ((q & 2) << 2) | ((p & 1) << 4) | ((q & 1) << 5)) / 64;
+		}
+		var threshold = 256 / 8;
 		this.each(function() {
 			if (this.tagName !== "IMG") return;
 			var canvas = document.createElement('canvas');
@@ -9,23 +15,27 @@
 			var height = canvas.height = this.height;
 			canvas.style.imageRendering = "-moz-crisp-edges";
 			this.style.imageRendering = "-moz-crisp-edges";
+			canvas.style.imageRendering = "-o-crisp-edges";
+			this.style.imageRendering = "-o-crisp-edges";
+			canvas.style.imageRendering = "-webkit-optimize-contrast";
+			this.style.imageRendering = "-webkit-optimize-contrast";
+			canvas.style.msInterpolationMode = "nearest-neighbor";
+			this.style.msInterpolationMode = "nearest-neighbor";
 			ctx.drawImage(this, 0, 0, width, height);
 			var imageData = ctx.getImageData(0, 0, width, height);
-			var threshold_map = [];
-			for (var p = 0; p < 64; ++p) {
-				var q = p ^ (p >> 3);
-				threshold_map[p] = (((p & 4) >> 2) | ((q & 4) >> 1) | ((p & 2) << 1) | ((q & 2) << 2) | ((p & 1) << 4) | ((q & 1) << 5)) / 64;
-			}
 			var pixel  = imageData.data;
-			var threshold = 256 / 8;
 			for (var y = 0; y < height; ++y) {
+				var ywidth = y * width;
+				var yAnd7 = y & 7;
 				for (var x = 0; x < width; ++x) {
-					var index = (y * width + x) * 4;
-					var map_value = threshold_map[(x & 7) + ((y & 7) << 3)];
+					var index = (ywidth + x) * 4;
+					var map_value = threshold_map[(x & 7) + (yAnd7 << 3)];
 					var map_valueXthreshold = map_value * threshold;
-					pixel[index + 0] = (pixel[index + 0] + map_valueXthreshold) >> 3 << 3;
-					pixel[index + 1] = (pixel[index + 1] + map_valueXthreshold) >> 3 << 3;
-					pixel[index + 2] = (pixel[index + 2] + map_valueXthreshold) >> 3 << 3;
+					pixel[index] = (pixel[index] + map_valueXthreshold) >> 3 << 3;
+					++index;
+					pixel[index] = (pixel[index] + map_valueXthreshold) >> 3 << 3;
+					++index;
+					pixel[index] = (pixel[index] + map_valueXthreshold) >> 3 << 3;
 				}
 			}
 			ctx.putImageData(imageData, 0, 0);
